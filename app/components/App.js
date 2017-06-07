3619dabd461ef30b7b11d694506eefdaa8268028
Jesse Croxford <takeflightsc@gmail.com>
2017-06-07 00:56:49 -0600

    initial react router working with render

--- app/components/App.js
+++ app/components/App.js
@@ -1,22 +1,27 @@
 import React, {Component} from 'react';
-import ReactRouter from 'react-router-dom';
+//import ReactRouter from 'react-router-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import CitySearchForm from './CitySearchForm';
-//var Router = ReactRouter.BrowserRouter;
-//var Route = ReactRouter.Route;
+import Forecast from './Forecast';
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="navbar">
-                    <h1>My Weather App</h1>
-                    <CitySearchForm flexDir="row"/>
+            <BrowserRouter>
+                <div className="container">
+                    {/*<div className="navbar">
+                        <h1>My Weather App</h1>
+                        <CitySearchForm flexDir="row"/>
+                    </div>*.}
+                    
+                    {/*<div className="home-container" style={{backgroundImage: "url('app/images/pattern.svg')"}}>
+                        <h1 className='header'>Enter a City and State</h1>
+                        <CitySearchForm flexDir='column' />
+                    </div> */}
+                    <Route render={function() {return <p>404 not found</p>}} />
+                    <Route path='/forecast' component={Forecast} />
                 </div>
-                <div className="home-container" style={{backgroundImage: "url('app/images/pattern.svg')"}}>
-                    <h1 className='header'>Enter a City and State</h1>
-                    <CitySearchForm flexDir='column' />
-                </div>
-            </div>
+            </BrowserRouter>
         )
     }
 };
---
+++ app/components/Forecast.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Forecast extends Component {
+  render() {
+    return (
+      <div>Forcast Component Test</div>
+    )
+  }
+}
+
+module.exports = Forecast;
