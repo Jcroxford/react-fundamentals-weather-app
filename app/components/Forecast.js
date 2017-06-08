7fe5ec009b6a84d6b9312ec022dad0df98971d04
Jesse Croxford <takeflightsc@gmail.com>
2017-06-07 21:37:59 -0600

    Forecast module now renders and console.logs weather

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
+import api from '../utils/api';
 
-class Forecast extends Component {
+class Forecast extends Component {  
+  componentDidMount() {
+    const location = queryString.parse(this.props.location.search);
+    api.getWeatherData(location.city)
+      .then( data => 
+          console.log(data)
+      );
+  }
 
-  
   render() {
     return (
       <div>Forcast Component Test</div>
--- app/utils/api.js
+++ app/utils/api.js
@@ -2,22 +2,58 @@ import axios from 'axios';
 
 const apiKey = '3af5d393e9826f8e9c08ee5f67571f39';
 
+function fetchCurrentWeather(location) {
+    const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/weather?q=${location}&type=accurate&APPID=${apiKey}`);
+    
+    return axios.get(encodedURI)
+        .then( res => res.data );
+}
+
+function fetchFiveDayWeather(location) {
+    const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&type=accurate&APPID=${apiKey}&cnt=5`);
+
+    return axios.get(encodedURI)
+        .then( res => res.data.list);
+}
+
+function handleError(err) {
+    console.warn(err);
+    return null;
+} 
+
 module.exports = {
-    fetchCurrentWeather: (location) => {
-        const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/weather?q=${location}&type=accurate&APPID=${apiKey}`);
-        
-        return axios.get(encodedURI)
-            .then( (res) => {
-                return res.data;
-            });
-    },
+    getWeatherData: location => {
+        return axios.all([
+            fetchCurrentWeather(location),
+            fetchFiveDayWeather(location)
+        ])
+            .then( data => {
+                const currentWeather = data[0];
+                const fiveDayWeather = data[1];
 
-    fetchFiveDayWeather: (location) => {
-        const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&type=accurate&APPID=${apiKey}&cnt=5`);
-
-        return axios.get(encodedURI)
-            .then( (res) => {
-                return res.data.list;
+                return {
+                    currentWeather,
+                    fiveDayWeather
+                }
             })
+            .catch(handleError);
     }
+
+    // fetchCurrentWeather: (location) => {
+    //     const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/weather?q=${location}&type=accurate&APPID=${apiKey}`);
+        
+    //     return axios.get(encodedURI)
+    //         .then( (res) => {
+    //             return res.data;
+    //         });
+    // },
+
+    // fetchFiveDayWeather: (location) => {
+    //     const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&type=accurate&APPID=${apiKey}&cnt=5`);
+
+    //     return axios.get(encodedURI)
+    //         .then( (res) => {
+    //             return res.data.list;
+    //         })
+    // }
 }
